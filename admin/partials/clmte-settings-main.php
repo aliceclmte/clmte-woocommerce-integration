7989503cbb3e65f26ec456b21cbde639ef586a67
FluffyKod <kod@heiman.se>
2020-11-30 13:43:44 +0100

    cleanup

--- admin/partials/clmte-settings-main.php
+++ admin/partials/clmte-settings-main.php
@@ -23,14 +23,6 @@ $settings = array(
             'type'      => 'text',
             'desc_tip'  => __( ' Your organisation\'s Organisation ID which can be found by creating an account and organisation at clmte.com', 'clmte')
         ),
-        // array(
-        //     'id'        => $prefix . 'placement',
-        //     'name'      => __( 'Placement', 'clmte' ), 
-        //     'type'      => 'select',
-        //     'class'     => 'wc-enhanced-select',
-        //     'options'   => $placement_types,
-        //     'desc_tip'  => __( ' The placement of the clmte offset box. Use the clmte-offset shortcode for custom placement.', 'clmte')
-        // ),
         array(
             'id'        => $prefix . 'custom_placement',
             'name'      => __( 'Custom Placement?', 'clmte' ),
@@ -38,18 +30,6 @@ $settings = array(
             'desc'  => __( 'Check this box to use custom placement with shortcode.', 'clmte' ),
             'default'   => 'no'
         ),   
-        // array(
-        //     'id'        => $prefix . 'compensation_price',
-        //     'name'      => __( 'Compensation Price', 'clmte' ), 
-        //     'type'      => 'number',
-        //     'desc_tip'  => __( ' The price of the compensation. Leave blank to automatically get the compensation price.', 'clmte')
-        // ), 
-        // array(
-        //     'id'        => $prefix . 'compensation_product_id',
-        //     'name'      => __( 'Compensation Product Id', 'clmte' ), 
-        //     'type'      => 'number',
-        //     'desc_tip'  => __( ' The id of the compensation product.', 'clmte')
-        // ), 
         array(
             'id'        => '',
             'name'      => __( 'General Configuration', 'clmte' ),
